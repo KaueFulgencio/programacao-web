870683c32aaa0db821848a489bd5a81192fa58ac
Kaue Fulgencio <Kauemfulgencio@outlook.com>
2023-10-09 15:09:04 -0300

    Criação da classe ficha e instrutor_parceiro

--- ProjetoDjango/projeto_final/app_projeto_final/models.py
+++ ProjetoDjango/projeto_final/app_projeto_final/models.py
@@ -3,4 +3,18 @@ from django.db import models
 class Usuario(models.Model):
     id_user = models.AutoField(primary_key=True)
     email = models.TextField(max_length=255)
+    nome = models.TextField(max_length=200)
     senha = models.TextField(max_length=255)
+    
+class Instrutor_Parceiro(models.Model):
+    cpf = models.CharField(max_length=12)
+    nome = models.CharField(max_length=50)
+    dt_entrada = models.DateField()
+    numero_alunos = models.IntegerField()
+
+class Ficha(models.Model):
+    id = models.AutoField(primary_key=True)
+    instrutor = models.ForeignKey(Instrutor_Parceiro, on_delete=models.CASCADE)
+    aluno_vinculado = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    categoria = models.CharField(max_length=20)
+    dt_created = models.DateField()
--- ProjetoDjango/projeto_final/app_projeto_final/views.py
+++ ProjetoDjango/projeto_final/app_projeto_final/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from .models import Usuario
-# Create your views here.
+
 def home(request):
     return render(request, 'usuarios/home.html')
 
 def usuarios(request):
     novo_usuario = Usuario()
     novo_usuario.email = request.POST.get('email') 
+    novo_usuario.nome = request.POST.get('nome')
     novo_usuario.senha = request.POST.get('senha') 
     novo_usuario.save()
     
